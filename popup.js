0fa8c82539cf43ba2b5f6d8f8b37010f92c119cd
SWbottier <samuelwaybespoke@gmail.com>
2017-08-08 23:51:33 +0800

    Commiting all YOUMARKER files to gitHub

---
+++ popup.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('main-form').addEventListener('submit', function(event){
+    event.preventDefault();
+    console.log('hi')
+
+  })
+});
